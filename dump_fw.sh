c9a23ba55053f6dd0dbedf6e6c11e534424e15e4
Mathias Neerup <mane@mmmi.sdu.dk>
2019-01-25 19:53:05 +0100

    Move files

--- dump_fw.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for chain in "$@"; do
-	sudo iptables -L $chain -v -n | column -t
-done
-
--- policy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-[ $QUIET = yes ] || echo "Catch anything else on the INPUT/OUTPUT/FORWARD chains..."
-
-$iptables -A INPUT   -j LOG --log-prefix "FW?L firewall Gate In: "  --log-level warning
-$iptables -A OUTPUT  -j LOG --log-prefix "FW?L firewall Gate Out: " --log-level warning
-$iptables -A FORWARD -j LOG --log-prefix "FW?L firewall Gate Fwd: " --log-level warning
